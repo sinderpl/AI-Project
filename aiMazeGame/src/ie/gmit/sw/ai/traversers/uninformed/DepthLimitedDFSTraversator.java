6c2cb303e04356e0c0429c0b8f55b1d3e7d71c6b
Christopher Weir <christopher.weir@hotmail.com>
2017-04-08 13:26:59 +0100

    Add Traversers and Maze Generation Algorithms

---
+++ aiMazeGame/src/ie/gmit/sw/ai/traversers/uninformed/DepthLimitedDFSTraversator.java
@@ -0,0 +1,50 @@
+package ie.gmit.sw.ai.traversers.uninformed;
+
+import ie.gmit.sw.ai.maze.*;
+import ie.gmit.sw.ai.traversers.Traversator;
+import ie.gmit.sw.ai.traversers.TraversatorStats;
+public class DepthLimitedDFSTraversator implements Traversator{
+	private Node[][] maze;
+	private int limit;
+	private boolean keepRunning = true;
+	private long time = System.currentTimeMillis();
+	private int visitCount = 0;
+	
+	public DepthLimitedDFSTraversator(int limit){
+		this.limit = limit;
+	}
+	
+	public void traverse(Node[][] maze, Node node) {
+		this.maze = maze;
+		System.out.println("Search with limit " + limit);
+		dfs(node, 1);
+	}
+	
+	private void dfs(Node node, int depth){
+		if (!keepRunning || depth > limit) return;
+		
+		node.setVisited(true);	
+		visitCount++;
+		
+		if (node.isGoalNode()){
+	        time = System.currentTimeMillis() - time; //Stop the clock
+	        TraversatorStats.printStats(node, time, visitCount);
+	        keepRunning = false;
+			return;
+		}
+		
+		try { //Simulate processing each expanded node
+			Thread.sleep(50);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		Node[] children = node.children(maze);
+		for (int i = 0; i < children.length; i++) {
+			if (children[i] != null && !children[i].isVisited()){
+				children[i].setParent(node);
+				dfs(children[i], depth + 1);
+			}
+		}
+	}
+}
